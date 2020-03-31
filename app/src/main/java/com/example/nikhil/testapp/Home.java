eb4f0dc6180509c0fa298488ac0895208b078b58
NikhilBartwal <nikhilbartwal1234@gmail.com>
2020-03-31 17:17:28 +0530

    Invoke steps fragment from home->walk view

--- app/src/main/java/com/example/nikhil/testapp/Home.java
+++ app/src/main/java/com/example/nikhil/testapp/Home.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Home extends Fragment {
@@ -41,7 +44,16 @@ public class Home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        CardView walkCardView = rootView.findViewById(R.id.walkCardview);
+        walkCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewPager viewpager = getActivity().findViewById(R.id.pager);
+                viewpager.setCurrentItem(2);
+            }
+        });
         return rootView;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
